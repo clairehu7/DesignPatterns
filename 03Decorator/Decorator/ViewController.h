8f0eed50f4eb998d04431f749cd9dc8af6c6e3d9
clairehu <471339423@qq.com>
2016-02-16 23:28:29 +0800

    [装饰模式] 创建文件

---
+++ 03Decorator/Decorator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Decorator
+//
+//  Created by 56QQ on 16/2/16.
+//  Copyright © 2016年 HKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
