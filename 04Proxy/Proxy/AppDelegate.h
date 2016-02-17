085ab3c0a786257d7ded0e8310a644595440c037
clairehu <471339423@qq.com>
2016-02-17 22:08:49 +0800

    [代理模式] 创建工程

---
+++ 04Proxy/Proxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Proxy
+//
+//  Created by 56QQ on 16/2/17.
+//  Copyright © 2016年 HKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 04Proxy/Proxy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Proxy
+//
+//  Created by 56QQ on 16/2/17.
+//  Copyright © 2016年 HKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
